8a45dc7851f15d08523584239513e8d53ec8fe91
Marwin <contact@marwin-rodrigues.fr>
2025-02-21 00:14:02 +0100

    fix:Header-projets-to-projects

--- components/Headnav.vue
+++ components/Headnav.vue
@@ -3,7 +3,7 @@
        <NuxtLink to="/"> <h1 class="ml-[4%] font-Primary font-bold text-2xl text-dark-color hover:animate-pulse">MarwinR.</h1></NuxtLink>
         <ul class="flex justify-center gap-10">
             <li class="font-Secondary font-semibold text-lg text-dark-color opacity-100 hover:opacity-70"><NuxtLink to="veille">Veille</NuxtLink></li>
-            <li class="font-Secondary font-semibold text-lg text-dark-color opacity-100 hover:opacity-70"><NuxtLink to="projets/">Projets</NuxtLink></li>
+            <li class="font-Secondary font-semibold text-lg text-dark-color opacity-100 hover:opacity-70"><NuxtLink to="projects/">Projets</NuxtLink></li>
             <li class="font-Secondary font-semibold text-lg text-dark-color opacity-100 hover:opacity-70"><NuxtLink to="about">À Propos</NuxtLink></li>
         </ul>
         <div class="flex justify-center gap-[10%]">
