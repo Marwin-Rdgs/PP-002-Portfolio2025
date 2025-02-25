c4c7e3ef39de3c4ebd1da66392e48227a99709fb
Marwin <contact@marwin-rodrigues.fr>
2025-02-25 19:59:24 +0100

    feat:first-ebauche-Index

--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,13 @@
 <template>
+
+    <section class="h-full pt-[6%]">
+        <h1 class="text-center text-dark-color text-6xl font-Primary font-bold mx-[20%] z-10">Je suis <span class="font-extrabold text-primary-color z-10">Marwin</span>. Chef de projet & Indépendant en Stratégie Digital</h1>
+        <div class="flex justify-center -mt-[18%] z-0">
+            <img src="/imgs/Marwin/Marwin-index.png" alt="Image de Marwin RODRIGUES" class="z-0">
+        </div>
+    </section>
+
+    <section class="bg-dark-color">
+        <h2 class="text-light-color text-center">C’est en repoussant ses limites que l’on découvre qui l’on est vraiment</h2>
+    </section>
 </template>
