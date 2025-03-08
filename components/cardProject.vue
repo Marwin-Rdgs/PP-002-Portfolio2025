5024a16d7e975a594be47017f36a614b6d78cf8b
Marwin <contact@marwin-rodrigues.fr>
2025-03-08 15:28:16 +0100

    feat:firstEbauche-ProjectSection

--- components/cardProject.vue
+++ components/cardProject.vue
@@ -24,5 +24,10 @@ export default {
 </script>
 
 <template>
-    
+    <div class="group w-[128px] ">
+        <div class="relative">
+            <img src="/public/imgs/projects/emorphoz.gif" alt="" class="absolute w-full h-full z-0 grayscale group-hover:grayscale-0">
+            <h1 class="z-10">fldgkdfjkghjdkhjfjkdqhdjkshtest</h1>
+        </div>
+    </div>
 </template>
