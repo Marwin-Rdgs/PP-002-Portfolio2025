7ed3976053cbdec553ba3645bf39ae49385251d6
Marwin <contact@marwin-rodrigues.fr>
2025-03-09 11:10:38 +0100

    feat:addprojects-MAJ

--- pages/index.vue
+++ pages/index.vue
@@ -78,18 +78,18 @@ const avis = ref(1);
     <section class="desktop:mt-[4%] phone:mt-[15%]">
       <h1 class="text-5xl font-Primary font-bold text-primary-color ml-[2%]">Mon dernier projet</h1>
 
-      <div class="bg-[url(/imgs/projects/emorphoz.gif)] bg-cover bg-bottom mt-[1%] h-[100%]">
+      <div class="bg-[url(/imgs/projects/iutNFCTV.gif)] bg-cover bg-center mt-[1%] h-[100%]">
         <div class="grid items-center bg-light-color bg-opacity-5 rounded-r-full w-[50%] pl-[2%] pr-[4%] pt-[4%] h-[500px]">
-          <h2 class="text-5xl font-Primary font-bold text-primary-color">E-morphoz</h2>
-          <p class="text-lg font-Secondary font-medium text-light-color">E-morphoz est un générateur de NFT collaboratif où vous participez à la création de votre NFT en y ajoutant des accessoires. La température extérieure d’où vous êtes influe aussi sur le rendu final de la NFT. Une fois généré, une partie des bénéfices de la NFT vous sera reversée à vous et à une association à chaque revente.</p>
-          <RouterLink to="/projects/emorphoz"><button class="bg-primary-color text-dark-color rounded-full desktop:px-[24px] phone:px-[10px] phone:text-sm desktop:text-lg py-[1%] text-center font-Primary font-bold drop-shadow-lg hover:scale-110 hover:translate-x-6 hover:shadow-secondary-color hover:shadow-lg transition-all ease-in-out duration-500">Voir plus</button></RouterLink>
+          <h2 class="text-5xl font-Primary font-bold text-primary-color">iut NFC tv</h2>
+          <p class="text-lg font-Secondary font-medium text-light-color">Site répertoriant l’ensemble des contenus multimédias de l’Institut Universitaire et Technologique de Nord Franche-Comté.</p>
+          <RouterLink to="/projects/iutnfctv"><button class="bg-primary-color text-dark-color rounded-full desktop:px-[24px] phone:px-[10px] phone:text-sm desktop:text-lg py-[1%] text-center font-Primary font-bold drop-shadow-lg hover:scale-110 hover:translate-x-6 hover:shadow-secondary-color hover:shadow-lg transition-all ease-in-out duration-500">Voir plus</button></RouterLink>
         </div>
       </div>
       <div class="desktop:flex phone:flex-col desktop:flex-row gap-[2%] justify-center mx-[2%] mt-[1%]">
-        <CardProject title="e-morphoz" img="/imgs/projects/emorphoz.gif" alt="projet"/>
-        <CardProject title="e-morphoz" img="/imgs/projects/emorphoz.gif" alt="projet"/>
-        <CardProject title="e-morphoz" img="/imgs/projects/emorphoz.gif" alt="projet"/>
-        <CardProject title="e-morphoz" img="/imgs/projects/emorphoz.gif" alt="projet"/>
+        <CardProject title="Flyer - Tony RODRIGUES" img="/imgs/projects/Com-Tony.jpeg" alt="projet"/>
+        <CardProject title="Carte de visite - MToutFer" img="/imgs/projects/Com-MToutFer.jpeg" alt="projet"/>
+        <CardProject title="Flyer - Devred 1902" img="/imgs/projects/Com-Devred.jpeg" alt="projet"/>
+        <CardProject title="GoMatch" img="/imgs/projects/gomatch.gif" alt="projet"/>
       </div>
       <RouterLink to="/projects"><p class="underline text-right">Voir plus...</p></RouterLink>
     </section>
