be3ce7016d7bf08a515efa2e009b6f725635e9b3
Marwin <contact@marwin-rodrigues.fr>
2025-03-23 11:26:45 +0100

    fix:design-cardProject

--- components/cardProject.vue
+++ components/cardProject.vue
@@ -22,11 +22,11 @@ export default {
 </script>
 
 <template>
-    <RouterLink :to="link" class="group w-[128px] h-[180px] relative overflow-hidden phone:px-[50%] desktop:px-[10%] rounded-xl">
+    <RouterLink :to="link" class="group w-[128px] h-[180px] relative overflow-hidden phone:px-[50%] desktop:px-[10%] rounded-xl border-2 border-primary-color border-opacity-15">
         <!-- Image en arrière-plan -->
         <img :src="img" :alt="alt" class="absolute inset-0 w-full h-full object-cover grayscale group-hover:grayscale-0"/>
         <!-- Titre en bas -->
-        <h1 class="absolute inset-x-0 bottom-0 text-white text-center bg-black/50 p-2 z-10 font-Primary font-extrabold text-light-color">
+        <h1 class="absolute inset-x-0 bottom-0 text-white text-center bg-black/50 p-2 z-10 font-Primary font-extrabold text-primary-color bg-light-color bg-opacity-25">
             {{ title }}
         </h1>
     </RouterLink>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,13 @@ export default defineNuxtConfig({
     '@/assets/css/main.css',
     'animate.css/animate.min.css'
   ],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  compatibilityDate: '2025-03-22',
 });
