8199d383f74a38d9a037f03592be04083512cb8d
Marwin <contact@marwin-rodrigues.fr>
2025-04-13 14:41:21 +0200

    feat:beggins-AllProjectsFilter

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,3 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const categ = ref('all')
+
+function toggleCategory(value) {
+  categ.value = categ.value === value ? 'all' : value
+}
+</script>
+
+
 <template>
     <h1>Page de listing de mes projets</h1>
-</template>
+  
+    <!-- <div class="flex justify-center"> -->
+      <div class="grid grid-cols-4 gap-[4%] mx-[30%]">
+        <button @click="toggleCategory('scool'); console.log(categ)" class="bg-contrast-color bg-opacity-10 rounded-xl text-dark-color">Scolaire</button>
+        <button @click="toggleCategory('perso'); console.log(categ)" class="bg-contrast-color bg-opacity-10 rounded-xl text-dark-color">Personnel</button>
+        <button @click="toggleCategory('presta'); console.log(categ)" class="bg-contrast-color bg-opacity-10 rounded-xl text-dark-color">Prestation</button>
+        <button @click="toggleCategory('pro'); console.log(categ)" class="bg-contrast-color bg-opacity-10 rounded-xl text-dark-color">Professionnel</button>
+      </div>
+    <!-- </div> -->
+  </template>
+  
