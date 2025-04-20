044347dbaa287af1004b6f19ae710c0833c87e2b
Marwin <contact@marwin-rodrigues.fr>
2025-04-20 21:28:41 +0200

    feat:AddDynamicPageforProject

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,3 +1,14 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import projectListRaw from '@/data/projectList.json'
+
+const route = useRoute()
+const projectId = route.params.id
+
+const project = projectListRaw.find(p => p.id === projectId)
+</script>
+
 <template>
     <h1>Page de présentation d'un projet</h1>
+    <h2>{{ project.title }}</h2>
 </template>
