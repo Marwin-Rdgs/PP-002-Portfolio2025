e1e0fd5255891af2cad7ca8fa45e3e4f48934819
Marwin <contact@marwin-rodrigues.fr>
2025-04-26 16:45:51 +0200

    feat:addArticleSystem

--- components/cardArticle.vue
+++ components/cardArticle.vue
@@ -1,2 +1,16 @@
+<script setup>
+defineProps({
+  article: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
 <template>
+    <div class="grid grid-cols-2 group hover:animate-pulse">
+        <img src="/imgs/projects/footer_wave.jpg" alt="" class="col-span-2 rounded-2xl group-hover:animate-bounce">
+        <h2>{{article.title}}</h2>
+        <NuxtLink :to="`/chez-marwinUnion/articles/${article.id}`"><p>lire l'article</p></NuxtLink>
+    </div>
 </template>
--- pages/chez-marwinUnion/articles/[id].vue
+++ pages/chez-marwinUnion/articles/[id].vue
@@ -1,3 +1,14 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import articleListRaw from '@/data/CMU-articles.json'
+
+const route = useRoute()
+const articleId = route.params.id
+
+const article = articleListRaw.find(a => a.id === articleId)
+</script>
+
 <template>
-    
+    <h1>Page de présentation d'un article</h1>
+    <h2>{{ article.title }}</h2>
 </template>
