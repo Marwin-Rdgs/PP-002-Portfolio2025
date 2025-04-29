9ed82468dc28f3c79c3989dc3586ccd8d564b5f7
Marwin <contact@marwin-rodrigues.fr>
2025-04-29 22:09:49 +0200

    feat:ResponsiveforCard&AE

--- components/cardArticle.vue
+++ components/cardArticle.vue
@@ -8,9 +8,11 @@ defineProps({
 </script>
 
 <template>
-    <div class="grid grid-cols-2 group hover:animate-pulse">
-        <img src="/imgs/projects/footer_wave.jpg" alt="" class="col-span-2 rounded-2xl group-hover:animate-bounce">
-        <h2>{{article.title}}</h2>
-        <NuxtLink :to="`/chez-marwinUnion/articles/${article.id}`"><p>lire l'article</p></NuxtLink>
+    <div class="grid grid-cols-2 group gap-y-[6%] desktop:mx-[4%] phone:mx-[10%]">
+        <img src="/imgs/projects/footer_wave.jpg" alt="" class="col-span-2 rounded-2xl group-hover:animate-pulse">
+        <div class="col-span-2 flex justify-between items-center">
+          <h2>{{article.title}}</h2>
+          <NuxtLink :to="`/chez-marwinUnion/articles/${article.id}`"><p>lire l'article</p></NuxtLink>
+        </div>
     </div>
 </template>
--- pages/chez-marwinUnion/index.vue
+++ pages/chez-marwinUnion/index.vue
@@ -8,8 +8,8 @@ const articleList = ref(articleListRaw);
 
 <template>
     <section class="flex justify-center items-center gap-[2%] mt-[6%] animate-pulse w-[90%] mx-auto">
-        <img src="/imgs/Chez-MarwinUnion/CMU.svg" alt="" class="w-[128px] animate-fadeIn">
-        <h1 class="font-Primary text-8xl text-primary-color drop-shadow-lg animate-fadeIn">Chez Marwin-Union</h1>
+        <img src="/imgs/Chez-MarwinUnion/CMU.svg" alt="" class="desktop:w-[128px] phone:w-[64px] animate-fadeIn">
+        <h1 class="font-Primary desktop:text-8xl phone:text-2xl text-primary-color drop-shadow-lg animate-fadeIn">Chez Marwin-Union</h1>
     </section>
     <p class="text-justify mx-[10%] text-sm mt-[3%] mb-[6%]">Chez Marwin-Union, je propose une vision humaine et engagée de l’accompagnement professionnel. Fondée sur des valeurs de partage, d’écoute et de challenge, cette auto-entreprise évolue aux côtés de ses clients, en s’adaptant à leurs besoins réels. Aujourd’hui, elle rassemble deux pôles d’expertise : l’un dédié à la digitalisation de la communication, l’autre à la qualité de vie au travail. Deux approches complémentaires, avec un objectif commun : créer du lien, donner du sens, et avancer ensemble.</p>
     
@@ -24,11 +24,11 @@ const articleList = ref(articleListRaw);
         </div>
     </section>
 
-    <section class="grid grid-cols-2 mx-[4%] gap-[10%]">
+    <section class="grid desktop:grid-cols-2 mx-[4%] gap-[10%]">
 
         <div class="grid mx-auto gap-y-[4%]">
             <div class="flex justify-center items-center gap-[2%]">
-                <img src="/imgs/Chez-MarwinUnion/CMW.svg" alt="" class="w-[64px] animate-fadeIn">
+                <img src="/imgs/Chez-MarwinUnion/CMW.svg" alt="" class="desktop:w-[64px] phone:w-[32px] animate-fadeIn">
                 <h1 class="font-Primary text-xl text-CMW-primary drop-shadow-lg animate-fadeIn">Chez Marwin-Web</h1>
             </div>
             <img src="/imgs/projects/citadelle.jpg" alt="" class="w-full">
@@ -40,7 +40,7 @@ const articleList = ref(articleListRaw);
 
         <div class="grid mx-auto gap-y-[4%]">
             <div class="flex justify-center items-center gap-[2%]">
-                <img src="/imgs/Chez-MarwinUnion/CMTC.svg" alt="" class="w-[64px] animate-fadeIn">
+                <img src="/imgs/Chez-MarwinUnion/CMTC.svg" alt="" class="desktop:w-[64px] phone:w-[32px] animate-fadeIn">
                 <h1 class="font-Primary text-xl text-CMTC-primary drop-shadow-lg animate-fadeIn">Chez Marwin-TeamCare</h1>
             </div>
             <img src="/imgs/projects/citadelle.jpg" alt="" class="w-full">
