90e12a79225e60e636a8a120f06d753e3ad3b2ec
Marwin <contact@marwin-rodrigues.fr>
2025-05-08 20:40:54 +0200

    fix:LinkButton-ClosePopup-News

--- components/news.vue
+++ components/news.vue
@@ -46,13 +46,13 @@ onMounted(() => {
           Admis au master MSC Project Management & Business, je suis à la recherche d'une entreprise pour accueillir une alternance de 2 ans dans les alentours de Strasbourg
         </p>
         <div class="desktop:flex phone:grid justify-center gap-[20%] mt-6">
-          <RouterLink to="/contact">
-            <button class="bg-light-color rounded-full desktop:px-[64px] phone:px-[10px] phone:text-sm desktop:text-lg py-[4%] text-center font-Primary font-bold drop-shadow-lg hover:scale-110 hover:-translate-y-2 hover:shadow-secondary-color hover:shadow-lg transition-all ease-in-out duration-300">
+          <RouterLink to="/about">
+            <button @click="closePopup" class="bg-light-color rounded-full desktop:px-[64px] phone:px-[10px] phone:text-sm desktop:text-lg py-[4%] text-center font-Primary font-bold drop-shadow-lg hover:scale-110 hover:-translate-y-2 hover:shadow-secondary-color hover:shadow-lg transition-all ease-in-out duration-300">
               Voir mon profil
             </button>
           </RouterLink>
-          <RouterLink to="/about">
-            <button class="bg-light-color rounded-full desktop:px-[64px] phone:px-[10px] phone:text-sm desktop:text-lg py-[4%] text-center font-Primary font-bold drop-shadow-lg hover:scale-110 hover:-translate-y-2 hover:shadow-secondary-color hover:shadow-lg transition-all ease-in-out duration-300">
+          <RouterLink to="/contact">
+            <button @click="closePopup" class="bg-light-color rounded-full desktop:px-[64px] phone:px-[10px] phone:text-sm desktop:text-lg py-[4%] text-center font-Primary font-bold drop-shadow-lg hover:scale-110 hover:-translate-y-2 hover:shadow-secondary-color hover:shadow-lg transition-all ease-in-out duration-300">
               Me contacter
             </button>
           </RouterLink>
