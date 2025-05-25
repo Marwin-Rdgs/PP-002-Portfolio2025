c7549c9d436913992949af81b1a5b98145290da5
Marwin <contact@marwin-rodrigues.fr>
2025-05-25 13:50:54 +0200

    feax:NewStructurePageforChezMarwinUnion

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@ const projectList = ref(projectListRaw.slice(-5, -1).reverse());
       <!-- Texte bien au-dessus -->
       <h1 class="text-center text-dark-color phone:text-4xl desktop:text-6xl font-Primary font-bold mx-[20%] z-10">
         Je suis <span class="font-extrabold text-primary-color">Marwin</span>. 
-        Chef de projet & Entrepreneur.
+        Business Manager & Entrepreneur.
       </h1>
   
       <div class="flex justify-center -mt-[20%]">
