eb53bf830dae2cff7cb8873297a2f3420975bdf4
Marwin <contact@marwin-rodrigues.fr>
2025-07-01 20:56:23 +0200

    feat:StartEditComponentsCompetence

--- components/tools/angular.vue
+++ components/tools/angular.vue
@@ -1,3 +1,8 @@
 <template>
-    Angular
+  <a href="https://www.adobe.com/fr/creativecloud.html?mv=search&mv=search&mv2=paidsearch&sdid=D8F91K7Y&ef_id=Cj0KCQjwjJrCBhCXARIsAI5x66Xm6vqDWI_Zs22p0c9gll-TJhJJNRcxmbh_46s3RgdOTHwkoGBKhSQaAmdgEALw_wcB:G:s&s_kwcid=AL!3085!3!600183576440!e!!g!!adobe!1435912500!56537399459&gad_source=1&gad_campaignid=1435912500&gbraid=0AAAAADraYsK1moAuzj2RE1tSp62mOvIPa&gclid=Cj0KCQjwjJrCBhCXARIsAI5x66Xm6vqDWI_Zs22p0c9gll-TJhJJNRcxmbh_46s3RgdOTHwkoGBKhSQaAmdgEALw_wcB" target="_blank" rel="noopener noreferrer" class="group flex flex-col items-center w-16">
+    <div class="relative w-12 h-12 rounded-md shadow hover:shadow-lg transition duration-300 ease-in-out transform group-hover:scale-110">
+      <img src="/public/imgs/projects/tools/adobe.png" alt="Icône de NuxtJS" class="w-full h-full object-contain"/>
+    </div>
+    <p class="mt-1 text-sm text-gray-600 font-medium opacity-0 group-hover:opacity-100 animate-pulse transition-opacity duration-300"> Adobe </p>
+  </a>
 </template>
