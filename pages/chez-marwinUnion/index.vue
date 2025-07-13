0963345717923427eb93d3d6d9a7c4dbf2dc98f1
Marwin <contact@marwin-rodrigues.fr>
2025-07-13 15:28:59 +0200

    fix:RemovePartnersListinUnion

--- pages/chez-marwinUnion/index.vue
+++ pages/chez-marwinUnion/index.vue
@@ -4,10 +4,7 @@ import CardArticle from '@/components/cardArticle.vue';
 import articleListRaw from '@/Data/CMU-articles.json'
 import CarouselFeedback from '@/components/CarouselFeedback.vue';
 import feedbackListRaw from '@/Data/feedbackIndex.json'
-import partnerListRaw from '@/Data/CMTC-partners.json'
 import CarouselPartners from '@/components/CMTC-CarouselPartners.vue'
-
-const partnerList = ref(partnerListRaw)
 
 const feedbackList = ref(feedbackListRaw);
 const articleList = ref(articleListRaw);
@@ -24,10 +21,7 @@ const articleList = ref(articleListRaw);
         <p class="text-base font-Secondary text-dark-color text-justify">Mais Chez Marwin Union, c’est bien plus qu’un nom d’auto-entreprise : c’est une philosophie. Chaque projet naît d’une vraie collaboration entre vous, avec vos idées, vos envies, vos exigences — et moi, avec mon expertise, ma créativité et mon sens de l’écoute.</p>
         <p class="text-base font-Secondary text-dark-color text-justify">Deux ans plus tard, voyant les résultats concrets et la fidélité de mes clients, j’ai eu envie de me réinventer. En sortant du 100 % digital, j’ai lancé un deuxième service : <a href="#CMTC" class="hover:underline">Chez Marwin-TeamCare</a>.</p>
         <p class="text-base font-Secondary text-dark-color text-justify"><span class="font-bold">L’objectif</span> : accompagner les entreprises dans un autre domaine essentiel, souvent négligé — le bien-être de leurs équipes. Car une entreprise épanouie, c’est d’abord une équipe soudée et motivée.</p>
-    </div>
-    <!-- <p class="text-justify mx-[10%] text-sm mt-[3%] mb-[6%]">Chez Marwin-Union, je propose une vision humaine et engagée de l’accompagnement professionnel. Fondée sur des valeurs de partage, d’écoute et de challenge, cette auto-entreprise évolue aux côtés de ses clients, en s’adaptant à leurs besoins réels. Aujourd’hui, elle rassemble deux pôles d’expertise : l’un dédié à la digitalisation de la communication, l’autre à la qualité de vie au travail. Deux approches complémentaires, avec un objectif commun : créer du lien, donner du sens, et avancer ensemble.</p> -->
-    <!-- <p class="text-justify mx-[10%] text-sm mt-[3%] mb-[6%]">Donner vie à votre présence digitale, main dans la main.Chez Marwin-Web est le premier service né de Chez Marwin Union. Il est dédié à la communication digitale sous toutes ses formes : création de sites web promotionnels, identité visuelle, gestion de réseaux sociaux, design print, montage vidéo... Ici, pas de solutions standardisées. Je vous accompagne de A à Z dans la création d’une image qui vous ressemble, avec des outils modernes, un regard extérieur bienveillant et des résultats concrets. Chaque collaboration est pensée comme un projet commun : vous apportez la vision, je propose la structure, les idées, les solutions, et surtout une grande réactivité.</p> -->
-    
+    </div>    
     
     <section class="mb-[10%]">
         <h4 class="text-xl font-Primary text-primary-color uppercase font-semibold ml-[4%] mb-[2%]">articles</h4>
