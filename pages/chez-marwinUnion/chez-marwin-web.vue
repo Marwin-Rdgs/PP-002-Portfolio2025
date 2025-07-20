d6b268b457ed10038c6a0d6016aa98fd848598c4
Marwin <contact@marwin-rodrigues.fr>
2025-07-20 20:30:18 +0200

    fix:edit-TimelineInformation-Web

--- pages/chez-marwinUnion/chez-marwin-web.vue
+++ pages/chez-marwinUnion/chez-marwin-web.vue
@@ -23,8 +23,8 @@
             <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
               <div class="order-1 w-5/12"></div>
               <div class="order-1 w-5/12 px-1 py-4 text-right">
-                <p class="mb-3 text-base text-yellow-300">1-6 May, 2021</p>
-                <h4 class="mb-3 font-bold text-lg desktop:text-2xl">Registration</h4>
+                <p class="mb-3 text-base text-yellow-300">1er Etape</p>
+                <h4 class="mb-3 font-bold text-lg desktop:text-2xl">Première échange téléphonique</h4>
                 <p class="text-sm desktop:text-base leading-snug text-gray-50 text-opacity-100">
                   Pick your favourite event(s) and register in that event by filling the form corresponding to that
                   event. Its that easy :)
@@ -34,8 +34,8 @@
             <div class="mb-8 flex justify-between items-center w-full right-timeline">
               <div class="order-1 w-5/12"></div>
               <div class="order-1  w-5/12 px-1 py-4 text-left">
-                <p class="mb-3 text-base text-yellow-300">6-9 May, 2021</p>
-                <h4 class="mb-3 font-bold text-lg desktop:text-2xl">Participation</h4>
+                <p class="mb-3 text-base text-yellow-300">2ème étape</p>
+                <h4 class="mb-3 font-bold text-lg desktop:text-2xl">Rdv commercial</h4>
                 <p class="text-sm desktop:text-base leading-snug text-gray-50 text-opacity-100">
                   Participate online. The links for your registered events will be sent to you via email and whatsapp
                   groups. Use those links and show your talent.
@@ -45,8 +45,8 @@
             <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
               <div class="order-1 w-5/12"></div>
               <div class="order-1 w-5/12 px-1 py-4 text-right">
-                <p class="mb-3 text-base text-yellow-300"> 10 May, 2021</p>
-                <h4 class="mb-3 font-bold text-lg desktop:text-2xl">Result Declaration</h4>
+                <p class="mb-3 text-base text-yellow-300"> 3ème étape</p>
+                <h4 class="mb-3 font-bold text-lg desktop:text-2xl">Lancement du projet</h4>
                 <p class="text-sm desktop:text-base leading-snug text-gray-50 text-opacity-100">
                   The ultimate genius will be revealed by our judging panel on 10th May, 2021 and the resukts will be
                   announced on the whatsapp groups and will be mailed to you.
@@ -58,8 +58,8 @@
               <div class="order-1 w-5/12"></div>
 
               <div class="order-1  w-5/12 px-1 py-4">
-                <p class="mb-3 text-base text-yellow-300">12 May, 2021</p>
-                <h4 class="mb-3 font-bold  text-lg desktop:text-2xl text-left">Prize Distribution</h4>
+                <p class="mb-3 text-base text-yellow-300">4ème étape</p>
+                <h4 class="mb-3 font-bold  text-lg desktop:text-2xl text-left">Réalisation Projet</h4>
                 <p class="text-sm desktop:text-base leading-snug text-gray-50 text-opacity-100">
                   The winners will be contacted by our team for their addresses and the winning goodies will be sent at
                   their addresses.
@@ -67,7 +67,7 @@
               </div>
             </div>
           </div>
-          <img class="mx-auto -mt-36 desktop:-mt-36" src="https://user-images.githubusercontent.com/54521023/116968861-ef21a000-acd2-11eb-95ac-a34b5b490265.png" />
+          <img class="mx-auto -mt-[5%]" src="https://user-images.githubusercontent.com/54521023/116968861-ef21a000-acd2-11eb-95ac-a34b5b490265.png" />
         </div>
       </div>
     </div>
