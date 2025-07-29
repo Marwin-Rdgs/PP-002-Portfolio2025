8af4ac1475b6816fa09bdf9a53cca0cd68cb4ba9
Marwin <contact@marwin-rodrigues.fr>
2025-07-29 21:36:33 +0200

    feat:npmGenerate-Build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 export default defineNuxtConfig({
+  ssr: true, // SSR est requis pour `nuxt generate` mais pas de backend ici
+  target: 'static', // facultatif mais explicite
+  nitro: {
+    preset: 'static'
+  },
   css: [
     '@/assets/css/main.css',
     'animate.css/animate.min.css'
