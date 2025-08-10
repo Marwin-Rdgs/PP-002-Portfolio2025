7cf10d48c423d82b7a164148dbdc29597ba9eef0
Marwin <contact@marwin-rodrigues.fr>
2025-08-10 16:45:15 +0200

    feat:add-googlesearchConsole

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
   <body class="bg-light-color scroll-smooth">
-    <NuxtLayout />
+    <NuxtLayout>
+      <NuxtPage />
+  </NuxtLayout>
   </body>
 </template>
+
+<script setup>
+useHead({
+  meta: [
+    {
+      name: 'google-site-verification',
+      content: 'zc_i19IX1q3Aq2YqC0BsR329dFCr-S2Uu3EhecI1o70' // Ton code de vérification
+    }
+  ]
+})
+</script>
