c5befb041a319341de2472161aacd19c1d041cd0
Marwin <contact@marwin-rodrigues.fr>
2025-12-09 16:29:33 +0100

    feat:1stSection-Presentation

--- pages/agilia-technology.vue
+++ pages/agilia-technology.vue
@@ -1,2 +1,31 @@
 <template>
+
+    <!-- PRESENTATION -->
+
+    <section aria-label="Redirection à l'histoire de Marwin RODRIGUES" class="relative grid desktop:grid-cols-2 phone:mt-[60%] desktop:mt-[20%]">
+      <div class="grid gap-[6%] ml-[4%]">
+        <h3 class="font-Primary font-bold text-dark-color phone:text-2xl desktop:text-5xl">Agilia Technology, <span class="text-primary-color">Qui sommes-nous ?</span></h3>
+        <p class="font-Secondary text-dark-color desktop:text-lg phone:text-sm">Passionné par le digital depuis le collège, j’ai forgé un parcours hybride alliant développement web, design, communication, gestion de projet et entrepreneuriat. J’aime transformer une idée en réalité concrète, mêlant technique et vision stratégique pour créer des projets ambitieux, utiles et centrés sur l’humain.</p>
+        <NuxtLink to="/a-propos"><button class="bg-primary-color text-dark-color rounded-full desktop:px-[24px] phone:px-[10px] phone:text-sm desktop:text-lg py-[1%] text-center font-Primary font-bold drop-shadow-lg hover:scale-110 hover:translate-x-6 hover:shadow-secondary-color hover:shadow-lg transition-all ease-in-out duration-500">Savoir plus</button></NuxtLink>      
+      </div>
+
+      <img loading="lazy" src="/imgs/agilia/TeamAgilia.jpg" alt="Image de Marwin RODRIGUES" class="w-[90%]">
+
+    </section>
+
+    <!-- Carrières -->
+    <section>
+        <h3 class="font-Primary font-bold text-dark-color phone:text-2xl desktop:text-5xl">Mes opportunités <span class="text-primary-color">pour vous !</span></h3>
+    </section>
+
+    <!-- Ils me font confiance -->
+    <section>
+        <h3 class="font-Primary font-bold text-dark-color phone:text-2xl desktop:text-5xl">Ils me font <span class="text-primary-color">confiance</span></h3>
+    </section>
+
+    <!-- Me contacter -->
+    <section>
+        <h3 class="font-Primary font-bold text-dark-color phone:text-2xl desktop:text-5xl">Me<span class="text-primary-color"> contacter !</span></h3>
+    </section>
+
 </template>
