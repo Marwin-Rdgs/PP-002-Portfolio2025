bcdf48b1dc5af07fa34a8f26d7aef5609811fe16
Marwin <contact@marwin-rodrigues.fr>
2026-03-04 11:18:58 +0100

    feat:MAJ-parcours

--- pages/a-propos.vue
+++ pages/a-propos.vue
@@ -254,20 +254,7 @@ function updateWord() {
                             <div class="text-dark-color font-Secondary w-5/6">Au sein d’une entreprise jeune, dynamique et profondément humaine, j’aurai l’opportunité de développer un vrai rôle d’intrapreneur, entre développement commercial, recrutement et accompagnement des talents. Mon objectif ? Créer des liens durables avec les clients et les consultants, en m’appuyant sur des valeurs fortes : confiance, agilité et respect. Une aventure professionnelle qui s’annonce aussi exigeante qu’excitante !</div>
                         </div>
 
-                        <!-- Item #1 -->
-                        <div class="relative pl-8 sm:pl-32 py-6 group">
-                            <!-- Purple label -->
-                            <div class="font-Primary font-medium text-2xl text-primary-color mb-1 sm:mb-0">Ambassadeur</div>
-                            <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->
-                            <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:border-4 after:box-content after:border-secondary-color after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                                <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold font-Primary uppercase px-[2%] h-6 mb-3 sm:mb-0 text-light-color bg-secondary-color rounded-full">Octobre 2025 à aujourd'hui</time>
-                                <NuxtLink to="https://www.carte-klub.com/" target="_blank"><div class="text-xl font-bold font-Primary text-dark-color hover:underline">KLUB</div></NuxtLink>
-                            </div>
-                            <!-- Content -->
-                            <div class="text-dark-color font-Secondary w-5/6">Représentation de l’application KLUB sur le terrain : distribution de flyers, animation de stands et création de contenu pour promouvoir une appli qui aide les jeunes à profiter de réductions locales.</div>
-                        </div>
-
-                        <!-- Item #2 -->
+                                                <!-- Item #1 -->
                         <div class="relative pl-8 sm:pl-32 py-6 group">
                             <!-- Purple label -->
                             <div class="font-Primary font-medium text-2xl text-primary-color mb-1 sm:mb-0">Indépendant en Stratégie Digitale & QVT</div>
@@ -279,6 +266,19 @@ function updateWord() {
                             <!-- Content -->
                             <div class="text-dark-color font-Secondary w-5/6">Auto-entreprise à double service, Chez Marwin-web pour la digitalisation de mes clients et Chez Marwin TeamCare pour l'organisation d'événements sur mesure pour entreprise via un réseau de partenaire pour booster la cohésion des équipes et éviter le turnover.</div>
                             <NuxtLink to="/chez-marwinUnion" class="hover:underline hover:animate-pulse">>> En savoir plus</NuxtLink>
+                        </div>
+
+                        <!-- Item #2 -->
+                        <div class="relative pl-8 sm:pl-32 py-6 group">
+                            <!-- Purple label -->
+                            <div class="font-Primary font-medium text-2xl text-primary-color mb-1 sm:mb-0">Ambassadeur</div>
+                            <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->
+                            <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:border-4 after:box-content after:border-secondary-color after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                                <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold font-Primary uppercase px-[2%] h-6 mb-3 sm:mb-0 text-light-color bg-secondary-color rounded-full">Octobre 2025 à Février 2026</time>
+                                <NuxtLink to="https://www.carte-klub.com/" target="_blank"><div class="text-xl font-bold font-Primary text-dark-color hover:underline">KLUB</div></NuxtLink>
+                            </div>
+                            <!-- Content -->
+                            <div class="text-dark-color font-Secondary w-5/6">Représentation de l’application KLUB sur le terrain : distribution de flyers, animation de stands et création de contenu pour promouvoir une appli qui aide les jeunes à profiter de réductions locales.</div>
                         </div>
 
                         <!-- Item #3 -->
@@ -332,7 +332,7 @@ function updateWord() {
                             <div class="font-Primary font-medium text-2xl text-primary-color mb-1 sm:mb-0">Responsable Développement web &<br /> Communication</div>
                             <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->
                             <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:border-4 after:box-content after:border-secondary-color after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                                <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold font-Primary uppercase px-[2%] h-6 mb-3 sm:mb-0 text-light-color bg-secondary-color rounded-full">Février 2023 à aujourd'hui</time>
+                                <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold font-Primary uppercase px-[2%] h-6 mb-3 sm:mb-0 text-light-color bg-secondary-color rounded-full">Février 2023 à Janvier 2026</time>
                                 <div class="text-xl font-bold font-Primary text-dark-color">Jadyss</div>
                             </div>
                             <!-- Content -->
